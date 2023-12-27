50efa1d0a87f7c424e2ba143b5d5548a85be8a58
bombin78 <bombin78@mail.ru>
2023-12-28 01:19:01 +0600

    add recursive binary search

--- binarySearch.js
+++ binarySearch.js
@@ -1,11 +1,9 @@
 // Бинарный поиск со сложностью алгоритма O(log n). 
 // Работает только с отсортированными данными
 const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 22, 23, 24, 25, 26, 27, 28, 39, 40]; // 20 элементов
-let count;
+let count = 0;
 
 function binarySearch(arr, item) {
-	count = 0;
-
 	let low = 0;
 	let high = arr.length - 1;
 	let mid;
@@ -28,7 +26,27 @@ function binarySearch(arr, item) {
 	return position;
 }
 
-console.log('position for 10 =', binarySearch(arr, 10));
-console.log('count for 10 =', count);
-console.log('position for 101 =', binarySearch(arr, 101));
-console.log('count for 101 =', count);
+function recursiveBinarySearch(arr, item, low, high) {
+	let mid = Math.floor((low + high)/2);
+	count++;
+
+	if(low >= high) return -1;
+
+	if(arr[mid] === item) return mid;
+
+	if(arr[mid] > item) {
+		return recursiveBinarySearch(arr, item, 0, mid - 1);
+	} else {
+		return recursiveBinarySearch(arr, item, mid + 1, high);
+	}
+}
+
+// console.log('position for 11 =', binarySearch(arr, 11));
+// console.log('count for 11 =', count);
+// console.log('position for 101 =', binarySearch(arr, 101));
+// console.log('count for 101 =', count);
+
+console.log('position for 11 =', recursiveBinarySearch(arr, 11, 0, arr.length - 1));
+console.log('count for 11 =', count);
+// console.log('position for 101 =', recursiveBinarySearch(arr, 101, 0, arr.length));
+// console.log('count for 101 =', count);
