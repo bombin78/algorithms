8a0ac7a8c3986ec7030c2746dbe9822fe98f1329
bombin78 <bombin78@mail.ru>
2024-01-10 19:46:07 +0600

    quickSort: add built-in sort function and function using forEach method

--- quickSort.js
+++ quickSort.js
@@ -1,8 +1,17 @@
 // Быстрая сортировка (сортировка Хоара) со сложностью алгоритма O(log n * n)
-const arr = [9, 3, 24, 1, 0, 7, 12, 11, 7, 8, 9, 17, 5, 4, 10, 23, 20, 19, 17, 15, 2, 45, 44, -8, 13, 30, 22, 33, 24, 26]; // 30 элементов
-let count = 0;
+const arr = [9, 3, 24, 1, 0, 7, 12, 11, 7, 8, 9, 17, 5, 4, 10, 23, 20, 17, 15, 2, 45, 44, -8, 13, 30, 22, 33, 24, 26]; // 30 элементов
+const arr2 = [999, ...arr];
+arr.push(888);
 
-function quickSort(arr) {
+let count1;
+let count2 = 0;
+
+arr2.sort((a,b) => {
+	count2++;
+	return a - b;
+});
+
+function quickSort1(arr) {
 	if(arr.length <= 1) {
 		return arr;
 	}
@@ -13,7 +22,7 @@ function quickSort(arr) {
 	let greater = [];
 
 	for(let i = 0; i < arr.length; i++) {
-		count++;
+		count1++;
 
 		if(i === pivotIdx) continue;
 		if(arr[i] < pivot) {
@@ -21,10 +30,58 @@ function quickSort(arr) {
 		} else {
 			greater.push(arr[i]);
 		}
+		count2++;
 	}
 
-	return [...quickSort(less), pivot, ...quickSort(greater)];
+	return [...quickSort1(less), pivot, ...quickSort1(greater)];
 }
 
-console.log('sorting result =', quickSort(arr));
-console.log('count =', count);
+function quickSort2(arr) {
+	if(arr.length <= 1) {
+		return arr;
+	}
+
+	let pivotIdx = Math.floor(arr.length / 2);
+	let pivot = arr[pivotIdx];
+	let less = [];
+	let greater = [];
+
+	arr.forEach((item, idx) => {
+		count1++;
+
+		if(idx === pivotIdx) return;
+		if(item < pivot) {
+			less.push(item);
+		} else {
+			greater.push(item);
+		}
+		count2++;
+	});
+
+	return [...quickSort2(less), pivot, ...quickSort2(greater)];
+}
+
+console.log('built-in sort function:');
+console.log('result =', arr2);
+console.log('Количество элементов =', arr2.length);
+console.log('Количество итераций = не известно');
+console.log('Вызов блока сравнения =', count2);
+console.log('-----------');
+
+count1 = 0;
+count2 = 0;
+console.log('quickSort1:');
+console.log('result =', quickSort1(arr));
+console.log('Количество элементов =', arr.length);
+console.log('Количество итераций =', count1);
+console.log('Вызов блока сравнения =', count2);
+console.log('-----------');
+
+count1 = 0;
+count2 = 0;
+console.log('quickSort2:');
+console.log('result =', quickSort2(arr));
+console.log('Количество элементов =', arr.length);
+console.log('Количество итераций =', count1);
+console.log('Вызов блока сравнения =', count2);
+console.log('-----------');
